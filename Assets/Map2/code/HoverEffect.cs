229470e35783bfd0753189de2e1b2c43bb95a121
haoon3104 <haoogpc08270@gmail.com>
2024-11-19 15:56:22 +0700

    NPC

---
+++ Assets/Map2/code/HoverEffect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class HoverEffect : MonoBehaviour
+{
+    public float hoverSpeed = 2.0f; // Tốc độ nhún
+    public float hoverHeight = 0.5f; // Biên độ nhún
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        // Lưu vị trí ban đầu
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Tạo chuyển động lên xuống
+        float newY = startPosition.y + Mathf.Sin(Time.time * hoverSpeed) * hoverHeight;
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
+}
+
