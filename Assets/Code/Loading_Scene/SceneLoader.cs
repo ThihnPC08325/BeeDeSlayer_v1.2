739225315048d6283c60659f397ce4cd8d634e08
ThihnPC08325 <thihnpc08325@fpt.edu.vn>
2024-12-02 16:57:12 +0700

    Thi
    Thêm script chuyển cảnh

---
+++ Assets/Code/Loading_Scene/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LevelLoader.Instance.LoadLevel(GetNextSceneIndex());
+        }
+    }
+
+    private int GetNextSceneIndex()
+    {
+        return SceneManager.GetActiveScene().buildIndex + 1;
+    }
+}
