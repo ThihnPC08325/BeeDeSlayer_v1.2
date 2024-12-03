6e8f78e5eb5dace176390c735b4884825ba09c68
ThihnPC08325 <thihnpc08325@fpt.edu.vn>
2024-12-03 17:10:34 +0700

    Thi
    Thêm Script hội thoại với NPC (Dialogue System)

---
+++ Assets/Code/DialogueSystem/Dialogue_Interaction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Dialogue_Interaction : MonoBehaviour
+{
+    [SerializeField] private Camera MainCamera;
+    [SerializeField] private Dialogue dialogue;
+
+    [Tooltip("Khoảng cách để tương tác với NPC")]
+    [SerializeField] private float interactionDistance = 2f;
+
+    [Tooltip("Layer nào dùng để tương tác được")]
+    [SerializeField] private LayerMask interactableLayer;
+
+    private bool isInteracting = false;
+
+    void Update()
+    {
+        if (!isInteracting && Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(MainCamera.transform.position, MainCamera.transform.forward, out hit, interactionDistance, interactableLayer))
+            {
+                if (dialogue != null)
+                {
+                    isInteracting = true;
+                    dialogue.gameObject.SetActive(true);
+                    StartCoroutine(DisablePlayerInteraction());
+                }
+            }
+        }
+    }
+
+    IEnumerator DisablePlayerInteraction()
+    {
+        yield return new WaitUntil(() => dialogue == null || !dialogue.gameObject.activeSelf);
+        isInteracting = false;
+    }
+}
