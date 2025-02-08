96a7deb7a9857502fe64af1c7cae1540310b2147
Phạm Đức Huân <155977706+PhamDucHuan@users.noreply.github.com>
2025-02-09 01:40:22 +0700

    Map 3
    Update boss Throw Rock

--- Assets/Map3/Code/NemDa/BossSkill.cs
+++ Assets/Map3/Code/NemDa/BossSkill.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class BossSkill : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class BossSkill : MonoBehaviour
     [SerializeField] private float throwForce = 100f; // Lực ném đá
     [SerializeField] private float throwInterval = 5f; // Thời gian giữa các lần ném
     [SerializeField] private float delayBeforeThrow = 7f; // Thời gian trì hoãn trước khi bắt đầu ném đá
+    [SerializeField] private Animator animator; // Animator của boss 
+    private float timeReloadThrow;
     private Transform player;
 
     void Start()
@@ -32,21 +35,30 @@ public class BossSkill : MonoBehaviour
     {
         while (true)
         {
-            ThrowRock();
+            StartCoroutine(timeToThrow());
             yield return new WaitForSeconds(throwInterval);
         }
     }
 
-    void ThrowRock()
+    private IEnumerator timeToThrow()
     {
         if (rockPrefabs.Count > 0 && player != null && throwPoint != null)
         {
             // Lấy ngẫu nhiên một prefab từ danh sách
             GameObject selectedRockPrefab = rockPrefabs[Random.Range(0, rockPrefabs.Count)];
 
+            animator.Play("ThrowRock");
+
+            RuntimeAnimatorController controller = animator.runtimeAnimatorController;
+            AnimationClip[] clips = controller.animationClips;
+            foreach (AnimationClip clip in clips)
+            {
+                timeReloadThrow = clip.length;
+            }
+            yield return new WaitForSeconds(timeReloadThrow -= 1.85f);
             // Tạo viên đá
             GameObject rock = Instantiate(selectedRockPrefab, throwPoint.position, throwPoint.rotation);
-
+            
             // Gán mục tiêu cho viên đá nếu có script RockBehavior
             RockBehavior rockBehavior = rock.GetComponent<RockBehavior>();
             if (rockBehavior != null)
