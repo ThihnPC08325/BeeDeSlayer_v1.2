9d999c43736d629117176f8c87ef0ae72d2d0b5f
Phạm Đức Huân <155977706+PhamDucHuan@users.noreply.github.com>
2025-02-21 15:20:21 +0700

    Revert "Revert map 3"
    This reverts commit a30b8084b766372cbe9173ad0dad2c56d6c94d09.

--- Assets/Map3/Code/NemDa/BossSkill.cs
+++ Assets/Map3/Code/NemDa/BossSkill.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.XR;
 
 public class BossSkill : MonoBehaviour
 {
@@ -10,8 +9,6 @@ public class BossSkill : MonoBehaviour
     [SerializeField] private float throwForce = 100f; // Lực ném đá
     [SerializeField] private float throwInterval = 5f; // Thời gian giữa các lần ném
     [SerializeField] private float delayBeforeThrow = 7f; // Thời gian trì hoãn trước khi bắt đầu ném đá
-    [SerializeField] private Animator animator; // Animator của boss 
-    private float timeReloadThrow;
     private Transform player;
 
     void Start()
@@ -35,30 +32,21 @@ public class BossSkill : MonoBehaviour
     {
         while (true)
         {
-            StartCoroutine(timeToThrow());
+            ThrowRock();
             yield return new WaitForSeconds(throwInterval);
         }
     }
 
-    private IEnumerator timeToThrow()
+    void ThrowRock()
     {
         if (rockPrefabs.Count > 0 && player != null && throwPoint != null)
         {
             // Lấy ngẫu nhiên một prefab từ danh sách
             GameObject selectedRockPrefab = rockPrefabs[Random.Range(0, rockPrefabs.Count)];
 
-            animator.Play("ThrowRock");
-
-            RuntimeAnimatorController controller = animator.runtimeAnimatorController;
-            AnimationClip[] clips = controller.animationClips;
-            foreach (AnimationClip clip in clips)
-            {
-                timeReloadThrow = clip.length;
-            }
-            yield return new WaitForSeconds(timeReloadThrow -= 1.85f);
             // Tạo viên đá
             GameObject rock = Instantiate(selectedRockPrefab, throwPoint.position, throwPoint.rotation);
-            
+
             // Gán mục tiêu cho viên đá nếu có script RockBehavior
             RockBehavior rockBehavior = rock.GetComponent<RockBehavior>();
             if (rockBehavior != null)
