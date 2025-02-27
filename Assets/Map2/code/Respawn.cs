722ed13271bfd2945e8d5963b7e9f66561638356
Bao41nsw0rth <ltqb7122005@gmail.com>
2025-02-27 16:34:02 +0700

    Xóa đồ không dùng, thêm lốc xoáy, code dịch chuyển

---
+++ Assets/Map2/code/Respawn.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ResetPlayerPosition : MonoBehaviour
+{
+    public Transform player;  // Tham chiếu đến người chơi
+    private Vector3 initialPosition; // Lưu vị trí ban đầu
+
+    private void Start()
+    {
+        if (player != null)
+        {
+            initialPosition = player.position; // Lưu vị trí ban đầu của người chơi
+        }
+        else
+        {
+            Debug.LogError("Chưa gán đối tượng người chơi!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            ResetPosition();
+        }
+    }
+
+    private void ResetPosition()
+    {
+        CharacterController controller = player.GetComponent<CharacterController>();
+
+        if (controller != null)
+        {
+            controller.enabled = false; // Tắt CharacterController để đặt vị trí mới
+            player.position = initialPosition;
+            controller.enabled = true; // Bật lại CharacterController
+        }
+        else
+        {
+            player.position = initialPosition;
+        }
+    }
+}
