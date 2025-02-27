6d23533e43dc340984d19157eb4780431b0ebe8a
Vietd3130 <155973029+Vietd3130@users.noreply.github.com>
2025-02-28 01:28:14 +0700

    SuaMan1

---
+++ Assets/Man1/Code/ExitZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Kiểm tra nếu Player chạm vào
+        {
+            SceneManager.LoadScene("Map2"); // Thay "NextScene" bằng tên scene tiếp theo
+        }
+    }
+}
